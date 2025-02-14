3e098ff436279495af8785bf1a5e4eb2157795a4
dai-development <daidev0105@gmail.com>
2025-02-14 09:08:42 +0900

    MarketingHeaderの作成

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import { MarketingHeader } from "@/components/marketing-header";
+
+const MarketingPage = async () => {
+  return (
+    <main className="min-h-screen">
+      <MarketingHeader />
+      {/* <Hero /> */}
+      {/* <Features /> */}
+      {/* <CallToAction /> */}
+      {/* <Footer /> */}
+    </main>
+  );
+};
+
+export default MarketingPage;
