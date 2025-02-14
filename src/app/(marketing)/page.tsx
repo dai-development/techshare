a6bc4d40e66d9046898fede9babb2b429925e52f
dai-development <daidev0105@gmail.com>
2025-02-14 14:13:36 +0900

    LPの作成：最後まで

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,13 +1,17 @@
+import { Hero } from "@/components/hero";
+import { Footer } from "@/components/footer";
+import { Features } from "@/components/features";
+import { CallToAction } from "@/components/call-to-action";
 import { MarketingHeader } from "@/components/marketing-header";
 
 const MarketingPage = async () => {
   return (
     <main className="min-h-screen">
       <MarketingHeader />
-      {/* <Hero /> */}
-      {/* <Features /> */}
-      {/* <CallToAction /> */}
-      {/* <Footer /> */}
+      <Hero />
+      <Features />
+      <CallToAction />
+      <Footer />
     </main>
   );
 };
