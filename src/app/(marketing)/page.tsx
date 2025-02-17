20e32202f7d7fef2b8cb3afa9e465414830f4d3d
dai-development <daidev0105@gmail.com>
2025-02-17 19:56:51 +0900

    認証機能の作成

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,10 +3,12 @@ import { Footer } from "@/components/footer";
 import { Features } from "@/components/features";
 import { CallToAction } from "@/components/call-to-action";
 import { MarketingHeader } from "@/components/marketing-header";
+import { LoginModal } from "@/features/auth/components/login-modal";
 
 const MarketingPage = async () => {
   return (
     <main className="min-h-screen">
+      <LoginModal />
       <MarketingHeader />
       <Hero />
       <Features />
