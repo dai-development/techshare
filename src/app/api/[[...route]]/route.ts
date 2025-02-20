c981de9f79fd0ea1b5d474f53fee00636f30c7b9
dai-development <daidev0105@gmail.com>
2025-02-20 14:52:45 +0900

    記事作成機能の作成

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,13 +2,16 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { logger } from "hono/logger";
 
+import articles from "@/features/articles/server/route";
 import authentication from "@/features/auth/server/route";
 
 const app = new Hono().basePath("/api");
 
 app.use("*", logger());
 
-const routes = app.route("/authentication", authentication);
+const routes = app
+  .route("/authentication", authentication)
+  .route("/articles", articles);
 
 export const GET = handle(app);
 export const POST = handle(app);
